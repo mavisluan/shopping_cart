efa5107d9480e7cd4bc2e6c62e93a41e33a3c447
Yi Angelov <mavisluan83@gmail.com>
2018-08-16 10:09:58 -0700

    add static data

---
+++ src/static-data.js
@@ -0,0 +1,45 @@
+export const items = [
+    {
+        id: 0, 
+        src: require(`./icons/ipad-mini2.jpeg`),
+        name:'Apple iPad Mini 2 16GB', 
+        description: 'An iPad like no other. 16GB, WiFi, 4G',
+        price: 229,
+    },
+    {
+        id: 1, 
+        src: require(`./icons/ipad-mini2.jpeg`),
+        name:'Apple iPad Mini 2 32GB', 
+        description: 'Even larger than 16GB',
+        price: 279,
+    },
+    {
+        id: 2, 
+        src: require(`./icons/canon-t6i.jpg`),
+        name:'Canon T6i', 
+        description: 'DSLR camera with lots of megapixels',
+        price: 749,
+    },
+    {
+        id: 3, 
+        name:'Apple pencil', 
+        src: require(`./icons/apple-pencil.jpeg`),
+        description: 'the best tool to reach for when you need pixel‑perfect precision',
+        price: 99,
+    },
+    {
+        id: 4, 
+        name:'Eau Thermale Avene Thermal Spring Water, 10.1', 
+        src: require(`./icons/spring-water.jpg`),
+        description: 'Unique composition soothes and softens the skin and restores skin’s natural balance. ',
+        price: 18.5,
+    },
+    {
+        id: 5, 
+        name:'Leather Slim Credit Card Holder', 
+        src: require(`./icons/card-holder.png`),
+        description: '100% Genuine Leather, Id Card Case Holder Useful Purse with Neck Strap (Sky Blue) ',
+        price: 13.5,
+    },
+
+]

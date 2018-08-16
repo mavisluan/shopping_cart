5a0a1f624c98d8cb8bf4b81414c0447d9bc8e82e
Yi Angelov <mavisluan83@gmail.com>
2018-08-16 15:45:22 -0700

    handle AddToCart function

--- src/ItemsPage.js
+++ src/ItemsPage.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Item from './Item'
 
-const ItemsPage = ({items}) => (
+const ItemsPage = ({items, onAddToCart}) => (
     <div className='items'>
         {items.map(item => (
             <div key={item.id}>
                 <Item item={item}>
-                    <button>Add to Cart</button>
+                    <button onClick={() => onAddToCart(item.id)}>Add to Cart</button>
                 </Item>
             </div>
          ))}

c297d5f21c58177898e3ba44170de4c201584cfc
Yi Angelov <mavisluan83@gmail.com>
2018-08-16 10:26:53 -0700

    add main page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { items } from './static-data'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='app'>
+        <div className='nav'>
+          <span>Items</span>  
+          <span>Cart</span>
+        </div>
+        <div className='board'>
+          ItemsPage
+          CartPage
+        </div>
       </div>
     );
   }

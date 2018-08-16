0abb6987330eb44ed7c616f0a6b4189ffedaa282
Yi Angelov <mavisluan83@gmail.com>
2018-08-16 15:55:47 -0700

    build links between pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { items } from './static-data'
 import ItemsPage from './ItemsPage'
 import CartPage from './CartPage'
+import { Route, Link } from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
@@ -15,7 +16,7 @@ class App extends Component {
   }
 
   // count ids in cart, return {id: count, id: count}
-  handleCountItems = (cart) => cart.reduce(( itemsCount, id) => {
+  CountCartItems = (cart) => cart.reduce(( itemsCount, id) => {
     itemsCount[id] = itemsCount[id] || 0
     itemsCount[id] ++
     return itemsCount
@@ -23,7 +24,7 @@ class App extends Component {
     
 
   render() {
-    const itemsCount= this.handleCountItems(this.state.cart)
+    const itemsCount= this.CountCartItems(this.state.cart)
     // find the item by id 
     // add count property to item {id: num, name:'', count: num}
     // put the item object into the cartItems array
@@ -35,13 +36,18 @@ class App extends Component {
     return (
       <div className='app'>
         <div className='nav'>
-          <span>Items</span>  
-          <span>Cart</span>
-          <span>{this.state.cart}</span>
+          <span><Link to='/'>Items</Link></span>
+          <span><Link to='/cart'>Cart</Link></span> 
         </div>
         <div className='board'>
-          <ItemsPage items={items} onAddToCart={this.handleAddToCart}/>
-          <CartPage items={cartItems} />
+          <Route exact path='/' render={()=> (
+            <ItemsPage 
+              items={items} 
+              onAddToCart={this.handleAddToCart}/>
+          )} />
+          <Route path='/cart' render={() => (
+             <CartPage items={cartItems} />
+          )} />
         </div>
       </div>
     );

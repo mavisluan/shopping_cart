78ca4129a25b788294efa844f00f4fdf194a9663
Yi Angelov <mavisluan83@gmail.com>
2018-08-16 17:42:37 -0700

    style the buttons on CartPage

--- src/CartPage.js
+++ src/CartPage.js
@@ -7,7 +7,7 @@ const CartPage = ({items, onAddToCart, onRemoveFromCart}) => (
             <div key={item.id}>
                 <Item item={item}>
                     <div className='cart-button'>
-                        <button onClick={()=> onRemoveFromCart(item.id)}><i class="fas fa-minus"></i></button>
+                        <button onClick={()=> onRemoveFromCart(item.id)}><i className="fas fa-minus"></i></button>
                         <span>{item.count}</span>
                         <button onClick={()=> onAddToCart(item.id)}><i className="fas fa-plus"></i></button>
                     </div>
